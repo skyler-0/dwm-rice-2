3a6e8b18e4f831e934ce3a56785fe46d9d6887b7
skyler-0 <81177923+Sarvasv-0@users.noreply.github.com>
2025-12-18 13:05:07 +0530

    changes

--- config.def.h
+++ config.def.h
@@ -10,20 +10,22 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono-Regular:size=10" };
 static const char dmenufont[]       = "JetBrainsMono-Regular:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_urgborder[]   = "#ff0000";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
-};
+//static const char col_gray1[]       = "#222222";
+//static const char col_gray2[]       = "#444444";
+//static const char col_gray3[]       = "#bbbbbb";
+//static const char col_gray4[]       = "#eeeeee";
+//static const char col_cyan[]        = "#005577";
+//static const char col_urgborder[]   = "#ff0000";
+//static const char *colors[][3]      = {
+//	/*               fg         bg         border   */
+//	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+//	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+//	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
+//};
+#include "/home/skyler/.cache/wal/colors-wal-dwm.h"
 
 static const char *const autostart[] = {
+        "dwm-autostart", NULL,
 	"newlook", NULL,
 	"fixtouchpad", NULL,
 	"picom", NULL,
@@ -71,11 +73,24 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *browsercmd[] = { "firefox", NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *poweroffcmd[] = { "poweroff", NULL };
+static const char *rebootcmd[] = { "reboot", NULL };
 static const char *clipmenucmd[] = { "clipmenu", NULL };
+static const char *clipmenuurlcmd[] = { "clipmenu-url", NULL };
+//static const char *upvolume[] = {"wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "5%+", NULL};
+static const char *upvolume[] = {"pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL};
+//static const char *downvolume[] = {"wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "5%-", NULL};
+static const char *downvolume[] = {"pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL};
+//static const char *mutesoundoutput[] = {"wpctl", "set-mute", "@DEFAULT_AUDIO_SINK@", "toggle", NULL};
+static const char *mutesoundoutput[] = {"pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL};
+//static const char *mutesoundinput[] = {"wpctl", "set-mute", "@DEFAULT_AUDIO_SOURCE@", "toggle", NULL};
+static const char *mutesoundinput[] = {"pactl", "set-source-mute", "@DEFAULT_SOURCE@", "toggle", NULL};
+static const char *upbrightness[] = {"light", "-A", "5", NULL};
+static const char *downbrightness[] = {"light", "-U", "5", NULL};
+
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -88,15 +103,14 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_u, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,		        XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_p,      setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
+	{ MODKEY|ShiftMask,             XK_g,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
@@ -117,8 +131,18 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ControlMask,	        XK_q,      quit,           {0} },
 	{ MODKEY|ControlMask,           XK_s,      spawn,           {.v = poweroffcmd} },
+	{ MODKEY|ControlMask,           XK_r,      spawn,           {.v = rebootcmd} },
 	{ MODKEY,                       XK_x,      spawn,           {.v = browsercmd} },
 	{ MODKEY,                       XK_v,      spawn,           {.v = clipmenucmd} },
+	{ MODKEY|ShiftMask,                       XK_v,      spawn,           {.v = clipmenuurlcmd} },
+	{ MODKEY|ShiftMask,                       XK_s,      spawn,           SHCMD("flameshot gui")},
+	//{ MODKEY|ShiftMask,                       XK_s,      spawn,           SHCMD("spectacle")},
+	{0, XF86XK_AudioLowerVolume, spawn, {.v=downvolume}},
+	{0, XF86XK_AudioRaiseVolume, spawn, {.v=upvolume}},
+	{0, XF86XK_AudioMute, spawn, {.v=mutesoundoutput}},
+	{0, XF86XK_AudioMicMute, spawn, {.v=mutesoundinput}},
+	{0, XF86XK_MonBrightnessUp, spawn, {.v=upbrightness}},
+	{0, XF86XK_MonBrightnessDown, spawn, {.v=downbrightness}},
 };
 
 /* button definitions */
